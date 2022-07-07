21cbb60239cda8ebe43378c8e7d47f016cc27ea1
Nikolas <kolemazur@gmail.com>
2022-07-07 12:00:32 -0600

    Add getThoughtById

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -6,5 +6,13 @@ module.exports = {
       .then((thoughts) => res.json(thoughts))
       .catch((err) => res.status(500).json(err));
   },
-
+  getThoughtById(req, res) {
+    Thought.findOne({ _id: req.params.thoughtId })
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: 'Please enter valid ID.' })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
 };
