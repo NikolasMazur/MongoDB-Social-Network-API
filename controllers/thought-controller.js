bcd35a465fba852703b7de7a4d44b0352ecc5359
Nikolas <kolemazur@gmail.com>
2022-07-07 13:43:16 -0600

    Add findOneAndUpdate

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -30,4 +30,17 @@ module.exports = {
       .then(() => res.json({ message: 'Thought removed.' }))
       .catch((err) => res.status(500).json(err));
   },
+  updateThought(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: 'Please enter valid ID.' })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
 };
