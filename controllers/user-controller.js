263f8aaaaf88abe394dff5fdf2518b6a5bc78a84
Nikolas <kolemazur@gmail.com>
2022-07-07 18:23:54 -0600

    Add postUser

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -15,4 +15,9 @@ module.exports = {
       )
       .catch((err) => res.status(500).json(err));
   },
+  postUser(req, res) {
+    User.create(req.body)
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.status(500).json(err));
+  },
 };
